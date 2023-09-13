f4bfecac063d15cdf0bb80c767c28d6b451d983e
Christian Felix Purps <christian_felix.purps@hs-karlsruhe.de>
2023-09-13 14:51:35 +0200

    add vosk

--- karero-webserver/tools/globals.js
+++ karero-webserver/tools/globals.js
@@ -2,7 +2,7 @@ var globalStore = {
     filename: "",
     communicationLevel: ["only_verbal","only_nonverbal","verbal_and_nonverbal"], // or "random or order"
     currentCommunicationLevel: "",
-    communicationMode: "order",
+    communicationMode: "verbal_and_nonverbal",
     countVisits: 0,
     welcomeDistance: 2.0
   };
