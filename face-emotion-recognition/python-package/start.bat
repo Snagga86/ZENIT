bd7df382b8f2744734e80546100a817ec44b1a72
Christian Felix Purps <christian_felix.purps@hs-karlsruhe.de>
2024-04-16 15:06:38 +0200

    clean up 1

--- face-emotion-recognition/python-package/start.bat
+++ face-emotion-recognition/python-package/start.bat
@@ -3,6 +3,6 @@ REM Activate the Conda environment
 call conda activate test-conda-env
 REM pause
 REM Execute the desired file or command
-python shot_detect.py 
+python emotion-shot-service.py 
 
 REM pause
