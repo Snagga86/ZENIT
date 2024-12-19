5cb727b9af4bc81507eb6ebb8e2af36a8476dd88
Christian Felix Purps <christian_felix.purps@hs-karlsruhe.de>
2024-12-19 14:06:59 +0100

    cleanup

--- zenit-body/remotestart.bat
+++
@@ -1,4 +0,0 @@
-SET MecharmIp=192.168.0.102
-::ssh -t %MecharmIp% 'cd KARERO/karero-body; /usr/bin/python3.7 /home/ubuntu/KARERO/karero-body/KARERO_control.py'
-start cmd /k ssh %MecharmIp% "cd KARERO/karero-body; /usr/bin/python3.7 /home/ubuntu/KARERO/karero-body/KARERO_control.py"
-pause
